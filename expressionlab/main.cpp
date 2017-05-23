aef488f807e93df169c6bb3d51c2bcc55cfe30d0
XTao <vertextao@hotmail.com>
2017-05-23 21:13:21 +0800

    init commit

---
+++ expressionlab/main.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include "expression.h"
+int main() {
+    el::expression expr1;
+    expr1.from_string("(3 + 5 - 8 * 2 / 9) * 6 / (2 + 7-1)");
+    std::cout << expr1.to_string() << std::endl;
+    std::vector<el::s_token> postfix;
+    el::expression::infix2postfix(expr1.m_tokens, postfix);
+    expr1.set_tokens(std::move(postfix));
+    std::cout << expr1.to_string() << std::endl;
+
+    el::expr_tree et1;
+    et1.construct_from_postfix(expr1.m_tokens);
+
+    el::expression expr2;
+    expr2.from_string("6*(3 + 5 - 2 * 8 / 9)  / (7+2-1)");
+    std::cout << expr2.to_string() << std::endl;
+    el::expression::infix2postfix(expr2.m_tokens, postfix);
+    expr2.set_tokens(std::move(postfix));
+    std::cout << expr2.to_string() << std::endl;
+
+    el::expr_tree et2;
+    et2.construct_from_postfix(expr2.m_tokens);
+
+    std::cout << et1.is_isomorphism(et2) << std::endl;
+    
+    getchar();
+}
