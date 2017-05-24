9bcc643edf1d13601e116a81fdf79eae3bc5f12f
XTao <vertextao@hotmail.com>
2017-05-24 16:52:52 +0800

    test 1 more case

--- expressionlab/main.cpp
+++ expressionlab/main.cpp
@@ -3,7 +3,8 @@
 #include "expression.h"
 void test() {
     el::expression expr1;
-    expr1.from_string("(3 + 5 - 8 * 2 / 9) * 6 / (1 + (7 - 2))");
+    //expr1.from_string("(3 + 5 - 8 * 2 / 9) * 6 / (1 + (7 - 2))");
+    expr1.from_string("1 + 2 + 3");
     std::cout << expr1.to_string() << std::endl;
     std::vector<el::s_token> postfix;
     el::expression::infix2postfix(expr1.m_tokens, postfix);
@@ -14,7 +15,8 @@ void test() {
     et1.construct_from_postfix(expr1.m_tokens);
 
     el::expression expr2;
-    expr2.from_string("6 * (3 + 5 - 2 * 8 / 9)  / (7-2+1)");
+    //expr2.from_string("6 * (3 + 5 - 2 * 8 / 9)  / (7-2+1)");
+    expr2.from_string("3 + (1 + 2)");
     std::cout << expr2.to_string() << std::endl;
     el::expression::infix2postfix(expr2.m_tokens, postfix);
     expr2.set_tokens(std::move(postfix));
